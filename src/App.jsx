eaa98c636a146af942e2c616a8bf3baab031dfa9
Kenip <kenip3765@gmail.com>
2026-09-02 11:11:09 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quasi
+        et reiciendis quaerat saepe dolores corrupti accusantium impedit
+        delectus ad beatae veniam ullam molestiae nemo quisquam sapiente
+        blanditiis, corporis aliquid, facilis quas laborum deleniti alias quod!
+        Esse eaque, dolores quis incidunt, sequi ducimus quia possimus error
+        iure impedit in quasi, ab aperiam adipisci dolore. Facilis blanditiis
+        impedit facere, laboriosam assumenda animi similique eaque, ad quo ea
+        incidunt minus delectus nesciunt eos ullam, ex non sint quam pariatur
+        deserunt officiis? Porro dolorum error, veritatis sit fugit sed, omnis
+        ratione libero similique quibusdam voluptate magnam voluptatum doloribus
+        consequatur quas officiis minus, ex voluptas quo quasi assumenda dolore
+        distinctio. Sunt suscipit a, iusto, voluptas architecto rem ratione
+        aperiam autem porro placeat, optio quis maxime repudiandae provident
+        animi officia in? Numquam architecto possimus dicta a laudantium
+        distinctio porro voluptatum? Dolorum corrupti unde vero distinctio
+        cumque minima quis, ex repellendus mollitia vitae temporibus aliquam
+        deserunt consequatur sunt quam, illum debitis, obcaecati saepe suscipit.
+        Nemo necessitatibus, labore culpa molestias, dolor alias maxime dolore
+        ea perspiciatis sit eaque placeat enim eum beatae cupiditate. Voluptas
+        cum aliquid magnam modi dignissimos. Dignissimos laboriosam repudiandae
+        sapiente cumque porro praesentium itaque, ducimus debitis tempora
+        molestias sint fugiat expedita obcaecati eos! Quaerat.
+      </h1>
     </>
   );
 }
